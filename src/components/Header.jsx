dcc536a8d49198728c8e0f0cf47e66c2194a2613
Sameer Saleem <itsmetrax2.0@gmail.com>
2023-11-21 00:59:10 +0500

    UseCase Section Completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Welcome from './Welcome'
-
 const Header = () => {
   return (
     <div className='navbar'>
         <Link className='navbar-title' to='/'>Writealy</Link>
         <Link className='navbar-section' to='/product'> Product - </Link>
-        <Link className='navbar-section navbar-elements' to='/UseCases'> Use Cases - </Link>
+        <Link className='navbar-section navbar-elements' to='/usecase'> Use Cases - </Link>
         <Link className='navbar-section navbar-elements' to='/pricing'> Pricing </Link>
         <Link className='navbar-section navbar-elements' to='/pricing'> Customers </Link>
         <Link className='navbar-section navbar-elements' to='/pricing'> Resources - </Link>
         <button className='signin-btn'>Sign in</button>
-        <Welcome />
     </div>
   )
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Welcome from './Welcome'
+import UseCase from './UseCase';
+
+
+export default function LandingPage() {
+  return (
+    <div>
+        <Welcome />
+        <UseCase/>
+    </div>
+  )
+}
