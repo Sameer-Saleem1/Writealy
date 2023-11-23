78a62132ccfaafe77dc6816afbc0d258b1d267ed
Sameer Saleem <itsmetrax2.0@gmail.com>
2023-11-23 14:24:35 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
-import Header from './components/Header';
-import LandingPage from './components/LandingPage';
-import UseCase from './components/UseCase';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Welcome from './components/Welcome';
+import "./App.css";
+import Header from "./components/Header";
+import LandingPage from "./components/LandingPage";
+import UseCase from "./components/UseCase";
+import Welcome from "./components/Welcome";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-         <Route path="/" element={<LandingPage />} />
-         <Route path="/usecase" element={<UseCase/>} />
-         <Route path="/product" element={<Welcome/>} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/usecase" element={<UseCase />} />
+        <Route path="/product" element={<Welcome />} />
       </Routes>
     </Router>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import logo from "../assets/img/logo.png";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className='navbar'>
-        <Link className='navbar-title' to='/'>Writealy</Link>
-        <Link className='navbar-section' to='/product'> Product - </Link>
-        <Link className='navbar-section navbar-elements' to='/usecase'> Use Cases - </Link>
-        <Link className='navbar-section navbar-elements' to='/pricing'> Pricing </Link>
-        <Link className='navbar-section navbar-elements' to='/pricing'> Customers </Link>
-        <Link className='navbar-section navbar-elements' to='/pricing'> Resources - </Link>
-        <button className='signin-btn'>Sign in</button>
+    <div className="navbar">
+      <img src={logo} alt="logo" className="logo" />
+      <Link className="navbar-title" to="/">
+        Writealy
+      </Link>
+      <Link className="navbar-section" to="/product">
+        {" "}
+        Product -{" "}
+      </Link>
+      <Link className="navbar-section navbar-elements" to="/usecase">
+        {" "}
+        Use Cases -{" "}
+      </Link>
+      <Link className="navbar-section navbar-elements" to="/pricing">
+        {" "}
+        Pricing{" "}
+      </Link>
+      <Link className="navbar-section navbar-elements" to="/pricing">
+        {" "}
+        Customers{" "}
+      </Link>
+      <Link className="navbar-section navbar-elements" to="/pricing">
+        {" "}
+        Resources -{" "}
+      </Link>
+      <button className="signin-btn">Sign in</button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
